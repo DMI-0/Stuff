9c333834011404a1333d90107c0dc0b415f101e9
DMI-0 <dmitrym801@gmail.com>
2025-05-08 09:18:57 -0400

    Delete Final directory

--- Final/Levels.py
+++
@@ -1,6 +0,0 @@
-import pygame as pg
-class Tiled_Map(pg.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
-        super().__init__(groups)
-        self.image = surf
-        self.rect = self.image.get_rect(topleft=pos)
